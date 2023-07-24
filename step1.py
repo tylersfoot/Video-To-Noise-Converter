4d13a1b454c9cfe12cb03ed4969afd757c179ad6
tylersfoot <tylerdepa8@gmail.com>
2023-07-23 23:16:16 -0400

    working product!

--- step1.py
+++ step1.py
@@ -9,7 +9,7 @@ def video_to_bw(video, threshold=128):
     it then uses fl_image() to run a function on each frame, and ours takes the brightness of each pixel, and if it is above or below the threshold
     brightness, sets the pixel to pure black or pure white
     '''
-    
+
     video_bw = video.fx(mp.vfx.blackwhite)
     
     def apply_threshold(frame):
@@ -44,56 +44,23 @@ def generate_noise_image(video):
 
     return noise_image
 
-# def create_noise_video(output_video_path, noise_image, video_duration, fps):
-#     '''
-#     this function creates a video the same length, dimensions, and fps as the original video
-#     and sets each frame to be the random noise image we generated earlier
-#     '''
-#     # convert the noise image (PIL Image) to a numpy array and then to RGB format
-#     noise_array_rgb = np.array(noise_image.convert('RGB'))
+def combine_frames(bw_frame, noise_image):
+    # Convert the noise image (PIL Image) to a numpy array
+    noise_array = np.array(noise_image)
 
-#     # create an array of noise images with the same dimensions as the original video
-#     noise_images_rgb = [noise_array_rgb for _ in range(int(fps * video_duration))]
+    # Iterate through each pixel in the bw_frame
+    for y in range(bw_frame.shape[0]):
+        for x in range(bw_frame.shape[1]):
+            # Check if the pixel in the bw_frame is black (pixel value is 0)
+            if bw_frame[y, x, 0] == 0:
+                # Invert the color of the corresponding pixel in the noise image
+                noise_array[y, x] = 255 - noise_array[y, x]
 
-#     # create the ImageSequenceClip using the list of random images
-#     noise_clip = mp.ImageSequenceClip(noise_images_rgb, fps=fps)
-
-#     noise_clip.write_videofile(output_video_path, codec='libx264')
+    return noise_array
 
 def combine_videos(output_video_path, bw_video, noise_image):
-    '''
-    combines the black and white video and the noise image
-    for each frame in the black and white video, if the pixel is black (brightness is 0),
-    it inverts the color of the corresponding pixel in the noise image
-    '''
-    # save the black and white video frames as an array
-    bw_video_frames = list(bw_video.iter_frames())
-
-    def combine_frames(bw_frame, noise_frame):
-            # Convert the numpy array of the bw_frame to a PIL Image
-            bw_image = Image.fromarray(bw_frame)
-
-            # Convert the noise frame (PIL Image) to a numpy array
-            noise_array = np.array(noise_frame)
-
-            # Iterate through each pixel in the bw_frame
-            for y in range(bw_image.height):
-                for x in range(bw_image.width):
-                    # Check if the pixel in the bw_frame is black (pixel value is 0)
-                    if bw_image.getpixel((x, y)) == 0:
-                        # Invert the color of the corresponding pixel in the noise image
-                        noise_array[y, x] = 255 - noise_array[y, x]
-
-            # Convert the numpy array back to a PIL Image
-            combined_frame = Image.fromarray(noise_array)
-
-            return np.array(combined_frame)
-
-    # Create the final combined frames by iterating through each frame of bw_video and using the noise_image
-    combined_frames = [combine_frames(bw_frame, noise_image) for bw_frame in bw_video_frames]
-
-    # Create the final video using the combined frames
-    combined_clip = mp.ImageSequenceClip(combined_frames, fps=bw_video.fps)
+    # Go through each frame in the black and white video and combine it with the noise image
+    combined_clip = bw_video.fl_image(lambda frame: combine_frames(frame, noise_image))
 
     # Write the combined clip to the output video file
     combined_clip.write_videofile(output_video_path, codec='libx264')
@@ -105,6 +72,7 @@ def main():
     noise_image_name = 'noise.png'
     # noise_video_name = 'noisevideo.mp4'
     final_video_name = 'finalvideo.mp4'
+    bw_threshold = 128 # value from 0 to 255
     
     # elete all files in the output folder except for input.mp4
     files_to_keep = ['input.mp4']
